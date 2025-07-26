6e30c5cee252d5042890e90ff03393b4fafd8092
Henry <henry@conducivetech.com>
2025-07-26 16:08:50 +1000

    Fix deprecated torch.cuda and fix memory preallocation

--- yolox/utils/metric.py
+++ yolox/utils/metric.py
@@ -38,7 +38,7 @@ def occupy_mem(cuda_device, mem_ratio=0.9):
     total, used = get_total_and_free_memory_in_Mb(cuda_device)
     max_mem = int(total * mem_ratio)
     block_mem = max_mem - used
-    x = torch.tensor([[[0.0]]], dtype=torch.float32, device='cuda').expand(256, 1024, block_mem)
+    x = torch.zeros((256, 1024, block_mem), dtype=torch.float32, device='cuda')
     del x
     time.sleep(5)
 
